656b94541ace8cebf9f1378509e3e1c4a5548405
Cjcode2 <166953227+Cjcode2@users.noreply.github.com>
2024-04-14 05:21:44 +0800

    Add files via upload

---
+++ anvsry/script.js
@@ -0,0 +1,7 @@
+alert("Hi baby!");
+alert("Happy 2nd Anniversary <3");
+alert("I made a simple code for you to celebrate our anniversary. I hope you appreciate this effort. I love you and happy 2nd anniversary, baby :) ");
+alert("WARNING! Your account has been hacked. Please change your password immediately.");
+alert("WARNING! Your account has been hacked. Please change your password immediately.");
+alert("Just Kidding XD");
+
